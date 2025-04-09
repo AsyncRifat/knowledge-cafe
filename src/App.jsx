64caf817c2ccea711c7807644afbcb69b814a9a7
AsyncRifat <ibrahim3rifat@gmail.com>
2025-04-10 03:33:53 +0600

    knowledge cafe

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Blogs from './components/navbar/blogs/Blogs';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Blogs></Blogs>
+    </>
+  );
+}
+
+export default App;
